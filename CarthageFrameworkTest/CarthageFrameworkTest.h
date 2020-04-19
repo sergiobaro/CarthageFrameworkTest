67f4c2ce2edc02dc726e1a4edbf50ab4e461181b
Sergio Baró <serbarsim@gmail.com>
2020-04-19 10:56:56 +0200

    Initial Commit

---
+++ CarthageFrameworkTest/CarthageFrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  CarthageFrameworkTest.h
+//  CarthageFrameworkTest
+//
+//  Created by Sergio Baro on 19/04/2020.
+//  Copyright © 2020 Sergio Baro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarthageFrameworkTest.
+FOUNDATION_EXPORT double CarthageFrameworkTestVersionNumber;
+
+//! Project version string for CarthageFrameworkTest.
+FOUNDATION_EXPORT const unsigned char CarthageFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageFrameworkTest/PublicHeader.h>
+
+
